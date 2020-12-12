d8826aa1bacde739450a7a38d2642b3e3f56c8e8
sonam <sonams394@gmail.com>
2020-12-12 17:39:37 +0530

    plateform 1 create

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var plateform1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,6 +19,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    plateform1 = new Ground(400, 305, 300, 170)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -61,4 +63,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    plateform1.display();
 }
